ebe66d87f392185d1c9d9331ee5d6f5dabddf060
Harikrishna Patnala <harikrishna.patnala@gmail.com>
2021-02-05 17:56:14 +0530

    Added version number on new API parameters Used single method for multiple checks
    Added the case for restore VM where VM can be restored to new template. In that case VM's dynamic scalability needs to be updated based on new template

--- server/src/main/java/com/cloud/vm/UserVmManager.java
+++ server/src/main/java/com/cloud/vm/UserVmManager.java
@@ -20,6 +20,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import com.cloud.offering.ServiceOffering;
+import com.cloud.template.VirtualMachineTemplate;
 import org.apache.cloudstack.api.BaseCmd.HTTPMethod;
 import org.apache.cloudstack.framework.config.ConfigKey;
 
@@ -122,4 +124,7 @@ public interface UserVmManager extends UserVmService {
     void persistDeviceBusInfo(UserVmVO paramUserVmVO, String paramString);
 
     HashMap<Long, List<VmNetworkStatsEntry>> getVmNetworkStatistics(long hostId, String hostName, List<Long> vmIds);
+
+    Boolean checkIfDynamicScalingCanBeEnabled(ServiceOffering offering, VirtualMachineTemplate template, Long zoneId);
+
 }
