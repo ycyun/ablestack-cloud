26d819f26bd9f731d53940192c1987d6f4b82016
JS Choi <77760789+jschoiRR@users.noreply.github.com>
2021-08-17 13:04:21 +0900

    kiosk 파라미터 추가

--- ui/src/components/widgets/WallLinkUrl.vue
+++ ui/src/components/widgets/WallLinkUrl.vue
@@ -18,7 +18,7 @@
 <template>
   <a
     v-if="['vm'].includes($route.meta.name) && 'updateVirtualMachine' in $store.getters.apis"
-    :href="$store.getters.features.wallportalvmurl+'?var-vm='+resource.instancename"
+    :href="$store.getters.features.wallportalvmurl+'?kiosk&var-vm='+resource.instancename"
     target="_blank" >
     <a-button style="margin-left: 5px" shape="circle" type="" :size="size" :disabled="['Stopped', 'Error', 'Destroyed'].includes(resource.state)" >
       <a-icon type="global" />
