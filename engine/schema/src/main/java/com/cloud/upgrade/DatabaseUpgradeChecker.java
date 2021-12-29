2cd2a29c321988b0dac7ba60d137a0cef4288dac
sjyu <sjyu@ablecloud.io>
2021-12-29 10:23:39 +0900

    Revert "Merge pull request #78 from Dajeong-Park/ablestack-bronto"
    This reverts commit 30265f207b66fcae0a6698a7a3268b13ce352bf1, reversing
    changes made to a3f57a669a7611b3ac1303aebd32bfea8ec427cd.

--- engine/schema/src/main/java/com/cloud/upgrade/DatabaseUpgradeChecker.java
+++ engine/schema/src/main/java/com/cloud/upgrade/DatabaseUpgradeChecker.java
@@ -30,8 +30,8 @@ import java.util.Date;
 import javax.inject.Inject;
 
 import com.cloud.upgrade.dao.Upgrade41510to41520;
-// import com.cloud.upgrade.dao.Upgrade41600to41610;
-// import com.cloud.upgrade.dao.Upgrade41610to41700;
+import com.cloud.upgrade.dao.Upgrade41600to41610;
+import com.cloud.upgrade.dao.Upgrade41610to41700;
 import org.apache.cloudstack.utils.CloudStackVersion;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
