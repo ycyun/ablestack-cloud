7c989814c1f3a0222601f949fd23cd5e6e8c1cae
stardom3645 <stardom3645@gmail.com>
2022-06-06 13:18:24 +0900

    오토메이션 컨트롤러 한글 변환, 리스트 권한 변경

--- plugins/integrations/automation-service/src/main/java/org/apache/cloudstack/api/command/user/automation/controller/ListAutomationControllerCmd.java
+++ plugins/integrations/automation-service/src/main/java/org/apache/cloudstack/api/command/user/automation/controller/ListAutomationControllerCmd.java
@@ -38,7 +38,7 @@ import com.cloud.automation.controller.AutomationControllerService;
         description = "Lists Automation Controller",
         responseObject = AutomationControllerResponse.class,
         responseView = ResponseObject.ResponseView.Restricted,
-        authorized = {RoleType.Admin, RoleType.ResourceAdmin, RoleType.DomainAdmin, RoleType.User})
+        authorized = {RoleType.DomainAdmin})
 public class ListAutomationControllerCmd extends BaseListCmd {
     public static final Logger LOGGER = Logger.getLogger(ListAutomationControllerCmd.class.getName());
     public static final String APINAME = "listAutomationController";
