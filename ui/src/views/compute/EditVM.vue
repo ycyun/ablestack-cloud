ef908c86a8203a6924b8e1d57457e4811afb1778
sjyu1 <vovysj@gmail.com>
2022-08-17 17:01:50 +0900

    가상머신 편집에서 ha활성화 가능하도록 버그수정

--- ui/src/views/compute/EditVM.vue
+++ ui/src/views/compute/EditVM.vue
@@ -175,6 +175,7 @@ export default {
         displayname: this.resource.displayname,
         ostypeid: this.resource.ostypeid,
         isdynamicallyscalable: this.resource.isdynamicallyscalable,
+        haenable: this.resource.haenable,
         group: this.resource.group,
         securitygroupids: this.resource.securitygroup.map(x => x.id)
       })
