6df314d49d9f1c7c9d1a006a1ac931359f6ba5b5
JS Choi <77760789+jschoiRR@users.noreply.github.com>
2022-09-23 10:04:35 +0900

    소스 병합 후 누락 정보 변경 및 수정, cpehfs mount 시 커널정보 확인 시 오류 수정

--- plugins/hypervisors/kvm/src/main/java/com/cloud/hypervisor/kvm/storage/KVMStorageProcessor.java
+++ plugins/hypervisors/kvm/src/main/java/com/cloud/hypervisor/kvm/storage/KVMStorageProcessor.java
@@ -1235,15 +1235,6 @@ public class KVMStorageProcessor implements StorageProcessor {
                             throw new InternalErrorException("Error while mapping disk "+attachingDisk.getPath()+" on host");
                         }
                     }
-                    if(provider != null && !provider.isEmpty() && "ABLESTACK".equals(provider)){
-                        final String unmap = resource.unmapRbdDevice(attachingDisk);
-                        if (unmap == null) {
-                            attachingDisk.setPath(krbdpath + "/" + attachingDisk.getPath());
-                            s_logger.debug("RBD unmap device on host is: " + attachingDisk.getPath());
-                        } else {
-                            throw new InternalErrorException("Error while mapping disk "+attachingDisk.getPath()+" on host");
-                        }
-                    }
                 }
 
                 for (final DiskDef disk : disks) {
@@ -1366,9 +1357,9 @@ public class KVMStorageProcessor implements StorageProcessor {
                 final String unmap = resource.unmapRbdDevice(attachingDisk);
                 if (unmap == null) {
                     attachingDisk.setPath(krbdpath + "/" + attachingDisk.getPath());
-                    s_logger.debug("RBD unmap device on host is: " + attachingDisk.getPath());
+                    s_logger.debug("Glue Block unmap device on host is: " + attachingDisk.getPath());
                 } else {
-                    throw new InternalErrorException("Error while mapping disk "+attachingDisk.getPath()+" on host");
+                    throw new InternalErrorException("Error while Glue Block unmapping disk "+attachingDisk.getPath()+" on host");
                 }
             }
         }
