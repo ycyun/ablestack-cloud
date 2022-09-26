ee45f80d27a661ca7ad8d2a4fd84307c60ff1f0b
JS Choi <77760789+jschoiRR@users.noreply.github.com>
2022-09-26 16:09:32 +0900

    db upgrade test 5

--- engine/schema/src/main/resources/META-INF/db/schema-BrontotoCerato.sql
+++ engine/schema/src/main/resources/META-INF/db/schema-BrontotoCerato.sql
@@ -140,23 +140,12 @@ BEGIN
 DROP PROCEDURE IF EXISTS addCol;
 
 CREATE DEFINER=`cloud`@`%` PROCEDURE `cloud`.`addCol`(
-    IN tb_name VarChar(200),
-    IN col_name VarChar(200),
-    IN col_type VarChar(1000)
+  IN tb_name VarChar(200),
+  IN col_name VarChar(200),
+  IN col_type VarChar(1000)
 )
 BEGIN
-    IF NOT EXISTS (
-        SELECT * 
-        FROM INFORMATION_SCHEMA.COLUMNS
-        WHERE TABLE_NAME = tb_name 
-        AND TABLE_SCHEMA = 'cloud' 
-        AND COLUMN_NAME = col_name
-    )
-    THEN
-        SET @ddl = CONCAT(' ALTER TABLE ', tb_name,' ADD COLUMN ', col_name, ' ', col_type);
-        PREPARE STMT FROM @ddl;
-        EXECUTE STMT;
-  END IF;
-END
+
+    IF NOT EXISTS ( SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = tb_name AND TABLE_SCHEMA = 'cloud' AND COLUMN_NAME = col_name ) THEN SET @ddl = CONCAT(' ALTER TABLE ', tb_name,' ADD COLUMN ', col_name, ' ', col_type); PREPARE STMT FROM @ddl; EXECUTE STMT; END IF; END
 
 CALL cloud.addCol('storage_pool', 'krbd_path', 'VARCHAR(255) DEFAULT null')
