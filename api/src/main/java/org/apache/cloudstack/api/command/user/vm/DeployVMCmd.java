c4bf0c8ae6ee7e2e9b30a6e0551d51a507564986
YeoCheon Yun <ycyun@ycyun.xyz>
2022-10-17 16:35:08 +0900

    tpm 설정 안되는 문제 수정
    대소문자 수정 tpmVersion -> tpmversion
    기본값 입력부분 수정

--- api/src/main/java/org/apache/cloudstack/api/command/user/vm/DeployVMCmd.java
+++ api/src/main/java/org/apache/cloudstack/api/command/user/vm/DeployVMCmd.java
@@ -127,7 +127,7 @@ public class DeployVMCmd extends BaseAsyncCreateCustomIdCmd implements SecurityG
 
 
     @Parameter(name = ApiConstants.TPM_VERSION, type = CommandType.STRING, required = false, description = "Boot with TPM", since = "4.18.0.0")
-    private String tpmVersion;
+    private String tpmversion;
     //DataDisk information
     @ACL
     @Parameter(name = ApiConstants.DISK_OFFERING_ID, type = CommandType.UUID, entityType = DiskOfferingResponse.class, description = "the ID of the disk offering for the virtual machine. If the template is of ISO format,"
@@ -304,12 +304,12 @@ public class DeployVMCmd extends BaseAsyncCreateCustomIdCmd implements SecurityG
     }
 
     public ApiConstants.TpmVersion getTpmVersion() {
-        if (StringUtils.isNotBlank(tpmVersion)) {
+        if (StringUtils.isNotBlank(tpmversion)) {
             try {
-                String type = tpmVersion.trim().toUpperCase();
+                String type = tpmversion.trim().toUpperCase();
                 return ApiConstants.TpmVersion.valueOf(type);
             } catch (IllegalArgumentException e) {
-                String errMesg = "Invalid TpmVersion " + tpmVersion + "Specified for vm " + getName()
+                String errMesg = "Invalid TpmVersion " + tpmversion + "Specified for vm " + getName()
                         + " Valid values are: " + Arrays.toString(ApiConstants.BootType.values());
                 s_logger.warn(errMesg);
                 throw new InvalidParameterValueException(errMesg);
@@ -341,13 +341,13 @@ public class DeployVMCmd extends BaseAsyncCreateCustomIdCmd implements SecurityG
             customparameterMap.put(entry.getKey(),entry.getValue());
         }
         if(customparameterMap.containsKey(ApiConstants.TpmVersion.V2_0.toString())){
-            customparameterMap.put("tpmVersion", customparameterMap.get(ApiConstants.TpmVersion.V2_0.toString()));
-        }else if(customparameterMap.containsKey("tpmVersion")){
-            customparameterMap.put("tpmVersion", customparameterMap.get("tpmVersion"));
+            customparameterMap.put("tpmversion", customparameterMap.get(ApiConstants.TpmVersion.V2_0.toString()));
+        }else if(customparameterMap.containsKey("tpmversion")){
+            customparameterMap.put("tpmversion", customparameterMap.get("tpmversion"));
         }else if(getTpmVersion() != null){
-            customparameterMap.put("tpmVersion", getTpmVersion().toString());
+            customparameterMap.put("tpmversion", getTpmVersion().toString());
         }else{
-            customparameterMap.put("tpmVersion", "NONE");
+            customparameterMap.put("tpmversion", "NONE");
         }
 
         return customparameterMap;
