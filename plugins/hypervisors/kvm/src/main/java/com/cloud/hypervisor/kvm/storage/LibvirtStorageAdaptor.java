90ec559be44d29f5cc4e003310ca16d74eccea16
YeoCheon Yun <ycyun@ycyun.xyz>
2022-11-30 18:08:27 +0900

    불필요한 오류 메시지 debug level 수정

--- plugins/hypervisors/kvm/src/main/java/com/cloud/hypervisor/kvm/storage/LibvirtStorageAdaptor.java
+++ plugins/hypervisors/kvm/src/main/java/com/cloud/hypervisor/kvm/storage/LibvirtStorageAdaptor.java
@@ -239,8 +239,7 @@ public class LibvirtStorageAdaptor implements StorageAdaptor {
             try {
                 s_logger.warn("Refreshing storage pool " + pool.getName());
                 if (pool.getXMLDesc(0).contains("lvm2")) {
-                    s_logger.warn("pool.getName()" + pool.getName());
-                    s_logger.warn("pool.getInfo()" + pool.getInfo());
+                    s_logger.warn("pool.getName(): " + pool.getName());
                     var outout = Script.runSimpleBashScript("lvchange " + "-ay " + pool.getName() + "/" + volName);
                     s_logger.warn("lvchange output: " + outout);
                 }
