c98153d716505b9700e62fb0c3a917b4421756a8
YeoCheon Yun <ycyun@ycyun.xyz>
2022-11-30 17:30:23 +0900

    풀의 이름을 사용하여 vg이름으로 사용 시도. 아니면 org.libvirt.StoragePool@7ea1ccd0 이런식으로 나옴

--- plugins/hypervisors/kvm/src/main/java/com/cloud/hypervisor/kvm/storage/LibvirtStorageAdaptor.java
+++ plugins/hypervisors/kvm/src/main/java/com/cloud/hypervisor/kvm/storage/LibvirtStorageAdaptor.java
@@ -239,7 +239,7 @@ public class LibvirtStorageAdaptor implements StorageAdaptor {
             try {
                 s_logger.warn("Refreshing storage pool " + pool.getName());
                 if (pool.getXMLDesc(0).contains("lvm2")) {
-                    var outout = Script.runSimpleBashScript("lvchange " + "-ay " + pool + "/" + volName);
+                    var outout = Script.runSimpleBashScript("lvchange " + "-ay " + pool.getName() + "/" + volName);
                     s_logger.warn("llvchange output: " + outout);
                 }
 
