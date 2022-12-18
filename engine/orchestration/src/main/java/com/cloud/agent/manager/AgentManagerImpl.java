01ff2c183ecd66cb4977246e53ddc3870933e3e0
sjyu1 <vovysj@gmail.com>
2022-12-18 22:13:13 +0900

    test

--- engine/orchestration/src/main/java/com/cloud/agent/manager/AgentManagerImpl.java
+++ engine/orchestration/src/main/java/com/cloud/agent/manager/AgentManagerImpl.java
@@ -1886,7 +1886,7 @@ public class AgentManagerImpl extends ManagerBase implements AgentManager, Handl
         String oom_score = "";
         s_logger.info("cmd = "+cmd);
         try {
-            Process p = Runtime.getRuntime().exec("cmd.exe /C " + cmd);
+            Process p = Runtime.getRuntime().exec(cmd);
             p.waitFor();
             s_logger.info("p = "+p);
             s_logger.info("p.pid() = " + p.pid() +", p.exitValue() = "+p.exitValue());
