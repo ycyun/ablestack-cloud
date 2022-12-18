3d695cc8e366d4f8db9cf2b21714e72caacad20c
sjyu1 <vovysj@gmail.com>
2022-12-18 22:32:06 +0900

    test

--- engine/orchestration/src/main/java/com/cloud/agent/manager/AgentManagerImpl.java
+++ engine/orchestration/src/main/java/com/cloud/agent/manager/AgentManagerImpl.java
@@ -1880,13 +1880,16 @@ public class AgentManagerImpl extends ManagerBase implements AgentManager, Handl
     @Override
     public String getOomScore(String hostIp, String vmName) {
         s_logger.info("hostIp = "+hostIp);
-        String cmd = "ps -aux | grep "+ vmName +" | awk '{print $2}' | head -1";
-        String[] cmd2 = new String[]{ "ps -aux | grep "+ vmName, " | awk '{print $2}'", "| head -1" };
+        // String cmd = "ps -aux | grep "+ vmName +" | awk '{print $2}' | head -1";
+        String[] cmd = new String[]{ "ps -aux | grep "+ vmName, " | awk '{print $2}'", "| head -1" };
         String s = "";
         String oom_score = "";
         s_logger.info("cmd = "+cmd);
         try {
-            Process p = Runtime.getRuntime().exec(cmd);
+            ProcessBuilder processBuilder = new ProcessBuilder(cmd);
+            Process p = processBuilder.start();
+
+            // Process p = Runtime.getRuntime().exec(cmd);
             p.waitFor();
             s_logger.info("p = "+p);
             s_logger.info("p.pid() = " + p.pid() +", p.exitValue() = "+p.exitValue());
@@ -1903,13 +1906,13 @@ public class AgentManagerImpl extends ManagerBase implements AgentManager, Handl
             // s_logger.info("sb2 = "+sb2.toString());
             oom_score = br.readLine();
 
-            Process p2 = Runtime.getRuntime().exec(cmd2);
-            p2.waitFor();
-            s_logger.info("p = "+p2);
-            s_logger.info("p.pid() = " + p2.pid() +", p.exitValue() = "+p2.exitValue());
+            // Process p2 = Runtime.getRuntime().exec(cmd2);
+            // p2.waitFor();
+            // s_logger.info("p = "+p2);
+            // s_logger.info("p.pid() = " + p2.pid() +", p.exitValue() = "+p2.exitValue());
 
-            BufferedReader br2 = new BufferedReader(new InputStreamReader(p2.getInputStream()));
-            s_logger.info("br2 = "+br2.readLine());
+            // BufferedReader br2 = new BufferedReader(new InputStreamReader(p2.getInputStream()));
+            // s_logger.info("br2 = "+br2.readLine());
 
             // String rst = br.readLine();
             // s_logger.info("rst = "+rst);
