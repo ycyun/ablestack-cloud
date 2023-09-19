d0aa9f8dd6fc988f955325abd5b80746c6473bb5
Dajeong-Park <ekwjd7182@gmail.com>
2023-09-19 14:56:23 +0900

    패스워드 정책 프로세스 테스트코드 추가

--- utils/src/main/java/com/cloud/utils/mold/SecurityCheck.java
+++ utils/src/main/java/com/cloud/utils/mold/SecurityCheck.java
@@ -20,28 +20,16 @@
 package com.cloud.utils.mold;
 
 import java.nio.charset.StandardCharsets;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.apache.commons.codec.binary.Base64;
-
+import com.cloud.utils.PasswordGenerator;
 import com.cloud.utils.StringUtils;
 import com.cloud.utils.crypt.AeadBase64Encryptor;
 import com.cloud.utils.crypt.EncryptionException;
 
 public class SecurityCheck {
-    private static final char[] hexChars = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
-    private static String toHexString(byte[] b) {
-        StringBuffer sb = new StringBuffer();
-        for (int i = 0; i < b.length; i++) {
-            sb.append(hexChars[(b[i] >> 4) & 0x0f]);
-            sb.append(hexChars[(b[i]) & 0x0f]);
-        }
-        return sb.toString();
-    }
-    public static void main(String[] args) throws EncryptionException, NoSuchAlgorithmException {
+    public static void main(String[] args) throws EncryptionException {
         Map<String, String> resultMap = new HashMap<>();
         // Request (Request 및 Response에 포함된 민감한 문자열을 제거하는 프로세스가 정상적으로 동작하는지 확인)
         final String input = "name=SS1&provider=SMB&zoneid=5a60af2b-3025-4f2a-9ecc-8e33bf2b94e3&url=cifs%3A%2F%2F10.102.192.150%2FSMB-Share%2Fsowmya%2Fsecondary%3Fuser%3Dsowmya%26password%3DXXXXX%40123%26domain%3DBLR";
@@ -65,50 +53,14 @@ public class SecurityCheck {
         } catch (EncryptionException e){
             resultMap.put("encrypt", "false");
         }
-        // Sshkey (SSH 공개키로 fingerprint 검증하는 프로세스가 정상적으로 동작하는지 확인)
-        String rsaKey =
-            "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC2D2Cs0XAEqm+ajJpumIPrMpKp0CWtIW+8ZY2/MJCW"
-                + "hge1eY18u9I3PPnkMVJsTOaN0wQojjw4AkKgKjNZXA9wyUq56UyN/stmipu8zifWPgxQGDRkuzzZ6buk"
-                + "ef8q2Awjpo8hv5/0SRPJxQLEafESnUP+Uu/LUwk5VVC7PHzywJRUGFuzDl/uT72+6hqpL2YpC6aTl4/P"
-                + "2eDvUQhCdL9dBmUSFX8ftT53W1jhsaQl7mPElVgSCtWz3IyRkogobMPrpJW/IPKEiojKIuvNoNv4CDR6"
-                + "ybeVjHOJMb9wi62rXo+CzUsW0Y4jPOX/OykAm5vrNOhQhw0aaBcv5XVv8BRX test@testkey";
-        String storedRsaKey =
-            "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC2D2Cs0XAEqm+ajJpumIPrMpKp0CWtIW+8ZY2/MJCW"
-                + "hge1eY18u9I3PPnkMVJsTOaN0wQojjw4AkKgKjNZXA9wyUq56UyN/stmipu8zifWPgxQGDRkuzzZ6buk"
-                + "ef8q2Awjpo8hv5/0SRPJxQLEafESnUP+Uu/LUwk5VVC7PHzywJRUGFuzDl/uT72+6hqpL2YpC6aTl4/P"
-                + "2eDvUQhCdL9dBmUSFX8ftT53W1jhsaQl7mPElVgSCtWz3IyRkogobMPrpJW/IPKEiojKIuvNoNv4CDR6"
-                + "ybeVjHOJMb9wi62rXo+CzUsW0Y4jPOX/OykAm5vrNOhQhw0aaBcv5XVv8BRX";
-        rsaKey = new String(Base64.decodeBase64(rsaKey.getBytes()));
-        String[] key = rsaKey.split(" ");
-        String parsedKey = "";
-        parsedKey = key[0].concat(" ").concat(key[1]);
-        System.out.printf("%s%n", parsedKey);
-        String keys[] = parsedKey.split(" ");
-        byte[] keyBytes = Base64.decodeBase64(keys[1]);
-        MessageDigest md5 = null;
-        String fingerprint = "";
-        String sumString = "";
-        try {
-            md5 = MessageDigest.getInstance("MD5");
-            if (md5 != null) {
-                sumString = toHexString(md5.digest(keyBytes));
-            }
-            for (int i = 2; i <= sumString.length(); i += 2) {
-                fingerprint += sumString.substring(i - 2, i);
-                if (i != sumString.length())
-                    fingerprint += ":";
-            }
-            System.out.printf("%s%n", fingerprint);
-            if (storedRsaKey.equals(parsedKey) && "f6:96:3f:f4:78:f7:80:11:6c:f8:e3:2b:40:20:f1:14".equals(fingerprint)) {
-                System.out.printf("%s%n", "true");
-                resultMap.put("sshkey", "true");
+        // password (password 정책 관련 프로세스가 정상적으로 동작하는지 확인)
+        String password = PasswordGenerator.generateRandomPassword(8);
+        for (char c : password.toCharArray()) {
+            if (!Character.isDigit(c) && Character.isLowerCase(c) && Character.isUpperCase(c)) {
+                resultMap.put("password", "true");
             } else {
-                System.out.printf("%s%n", "false");
-                resultMap.put("sshkey", "false");
+                resultMap.put("password", "false");
             }
-        } catch (NoSuchAlgorithmException e) {
-            System.out.printf("%s%n", "catch");
-            resultMap.put("sshkey", "false");
         }
         System.out.printf("%s%n", resultMap);
     }
