0f253746796dbe3c89984f5a9713488e7ccfae98
Dajeong-Park <ekwjd7182@gmail.com>
2023-09-19 12:41:28 +0900

    인증서 생성 관련 로직 테스트

--- utils/src/main/java/com/cloud/utils/mold/SecurityCheck.java
+++ utils/src/main/java/com/cloud/utils/mold/SecurityCheck.java
@@ -20,8 +20,14 @@
 package com.cloud.utils.mold;
 
 import java.nio.charset.StandardCharsets;
+import java.security.KeyPair;
+import java.security.cert.X509Certificate;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+
+import org.apache.cloudstack.utils.security.CertUtils;
 
 import com.cloud.utils.StringUtils;
 import com.cloud.utils.crypt.AeadBase64Encryptor;
@@ -51,6 +57,24 @@ public class SecurityCheck {
             }
         } catch (EncryptionException e){
             resultMap.put("encrypt", "false");
+        }
+        // Certificate (보안기능에 적합한 알고리즘으로 암호화된 인증서가 정상적으로 생성되는지 확인)
+        try {
+            KeyPair caKeyPair = CertUtils.generateRandomKeyPair(1024);
+            X509Certificate caCertificate = CertUtils.generateV3Certificate(null, caKeyPair, caKeyPair.getPublic(), "CN=test", "SHA256WithRSAEncryption", 365, null, null);
+            final KeyPair clientKeyPair = CertUtils.generateRandomKeyPair(1024);
+            final List<String> domainNames = Arrays.asList("domain1.com", "www.2.domain2.com", "3.domain3.com");
+            final List<String> addressList = Arrays.asList("1.2.3.4", "192.168.1.1", "2a02:120b:2c16:f6d0:d9df:8ebc:e44a:f181");
+            final X509Certificate clientCert = CertUtils.generateV3Certificate(caCertificate, caKeyPair, clientKeyPair.getPublic(),
+                    "CN=domain.example", "SHA256WithRSAEncryption", 10, domainNames, addressList);
+            clientCert.verify(caKeyPair.getPublic());
+            if (clientCert.getIssuerDN().equals(caCertificate.getIssuerDN()) && clientCert.getSigAlgName().equals("SHA256WITHRSA")) {
+                resultMap.put("certificate", "true");
+            } else {
+                resultMap.put("certificate", "false");
+            }
+        } catch (Exception e) {
+            resultMap.put("certificate", "false");
         }
         System.out.printf("%s%n", resultMap);
     }
