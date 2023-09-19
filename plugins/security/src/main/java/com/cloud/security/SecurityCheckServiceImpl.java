78462bdf630ac1897967dc378df81fe7bbbaeb39
Dajeong-Park <ekwjd7182@gmail.com>
2023-09-19 10:55:43 +0900

    테스트 코드 작성

--- plugins/security/src/main/java/com/cloud/security/SecurityCheckServiceImpl.java
+++ plugins/security/src/main/java/com/cloud/security/SecurityCheckServiceImpl.java
@@ -22,7 +22,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.Executors;
--- utils/src/main/java/com/cloud/utils/mold/SecurityCheck.java
+++ utils/src/main/java/com/cloud/utils/mold/SecurityCheck.java
@@ -19,13 +19,26 @@
 
 package com.cloud.utils.mold;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.SignatureException;
+import java.security.cert.CertificateException;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.bouncycastle.operator.OperatorCreationException;
+
 import com.cloud.utils.StringUtils;
+import com.cloud.utils.crypt.AeadBase64Encryptor;
+import com.cloud.utils.crypt.EncryptionException;
 
 public class SecurityCheck {
-    public static void main(String[] args) {
+    // private static final ExecutorService executor = Executors.newFixedThreadPool(10, new NamedThreadFactory("SecurityCheckTest"));
+    // private static final ProcessRunner RUNNER = new ProcessRunner(executor);
+    public static void main(String[] args) throws NoSuchProviderException, NoSuchAlgorithmException, InvalidKeyException, CertificateException, SignatureException, OperatorCreationException, IOException {
         Map<String, String> resultMap = new HashMap<>();
         // Request (Request 및 Response 에 포함된 민감한 문자열을 제거하는지 확인)
         final String input = "name=SS1&provider=SMB&zoneid=5a60af2b-3025-4f2a-9ecc-8e33bf2b94e3&url=cifs%3A%2F%2F10.102.192.150%2FSMB-Share%2Fsowmya%2Fsecondary%3Fuser%3Dsowmya%26password%3DXXXXX%40123%26domain%3DBLR";
@@ -35,6 +48,42 @@ public class SecurityCheck {
             resultMap.put("request", "true");
         } else {
             resultMap.put("request", "false");
+        }
+        // // ThreadPool (제품에서 실행되는 쓰레드풀이 정상적으로 동작하는지 확인)
+        // ProcessResult process = RUNNER.executeCommands(Arrays.asList("sleep", "0"));
+        // if (process.getReturnCode() == 0) {
+        //     resultMap.put("thread", "true");
+        // } else {
+        //     resultMap.put("thread", "false");
+        // }
+        // // Certificate (인증서 등록 프로세스가 정상적으로 동작하는지 확인)
+        // KeyPair caKeyPair = CertUtils.generateRandomKeyPair(1024);
+        // X509Certificate caCertificate = CertUtils.generateV3Certificate(null, caKeyPair, caKeyPair.getPublic(), "CN=test", "SHA256WithRSAEncryption", 365, null, null);
+        // final KeyPair clientKeyPair = CertUtils.generateRandomKeyPair(1024);
+        // final List<String> domainNames = Arrays.asList("domain1.com", "www.2.domain2.com", "3.domain3.com");
+        // final List<String> addressList = Arrays.asList("1.2.3.4", "192.168.1.1", "2a02:120b:2c16:f6d0:d9df:8ebc:e44a:f181");
+
+        // final X509Certificate clientCert = CertUtils.generateV3Certificate(caCertificate, caKeyPair, clientKeyPair.getPublic(),
+        //         "CN=domain.example", "SHA256WithRSAEncryption", 10, domainNames, addressList);
+
+        // clientCert.verify(caKeyPair.getPublic());
+        // if (clientCert.getIssuerDN().equals(caCertificate.getIssuerDN()) && clientCert.getSigAlgName().equalsIgnoreCase("SHA256WITHRSA")) {
+        //     resultMap.put("certificate", "true");
+        // } else {
+        //     resultMap.put("certificate", "false");
+        // }
+        // Encrypt (보안 인증 기능에 사용되는 암복호화가 동작하는지 확인)
+        final String pwd = "managementkey";
+        AeadBase64Encryptor encryptor = new AeadBase64Encryptor(pwd.getBytes(StandardCharsets.UTF_8));
+        try {
+            final String decrypt = encryptor.decrypt("DlTJUG8rWFjOd3aoHtbBGEcQ/piovBzRJ/bnQ1FACLg=");
+            if (decrypt.equalsIgnoreCase("mold")) {
+                resultMap.put("encrypt", "true");
+            } else {
+                resultMap.put("encrypt", "false");
+            }
+        } catch (EncryptionException e){
+            resultMap.put("encrypt", "false");
         }
         System.out.printf("%s%n", resultMap);
     }
