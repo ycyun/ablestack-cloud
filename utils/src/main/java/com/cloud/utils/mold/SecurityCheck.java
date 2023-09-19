88bd64bb086d270aa0e09f549daa7ff3b0c83d99
Dajeong-Park <ekwjd7182@gmail.com>
2023-09-19 12:04:50 +0900

    소스 정리

--- utils/src/main/java/com/cloud/utils/mold/SecurityCheck.java
+++ utils/src/main/java/com/cloud/utils/mold/SecurityCheck.java
@@ -28,8 +28,6 @@ import com.cloud.utils.crypt.AeadBase64Encryptor;
 import com.cloud.utils.crypt.EncryptionException;
 
 public class SecurityCheck {
-    // private static final ExecutorService executor = Executors.newFixedThreadPool(10, new NamedThreadFactory("SecurityCheckTest"));
-    // private static final ProcessRunner RUNNER = new ProcessRunner(executor);
     public static void main(String[] args) throws EncryptionException {
         Map<String, String> resultMap = new HashMap<>();
         // Request (Request 및 Response 에 포함된 민감한 문자열을 제거하는지 확인)
@@ -41,35 +39,11 @@ public class SecurityCheck {
         } else {
             resultMap.put("request", "false");
         }
-        // // ThreadPool (제품에서 실행되는 쓰레드풀이 정상적으로 동작하는지 확인)
-        // ProcessResult process = RUNNER.executeCommands(Arrays.asList("sleep", "0"));
-        // if (process.getReturnCode() == 0) {
-        //     resultMap.put("thread", "true");
-        // } else {
-        //     resultMap.put("thread", "false");
-        // }
-        // // Certificate (인증서 등록 프로세스가 정상적으로 동작하는지 확인)
-        // KeyPair caKeyPair = CertUtils.generateRandomKeyPair(1024);
-        // X509Certificate caCertificate = CertUtils.generateV3Certificate(null, caKeyPair, caKeyPair.getPublic(), "CN=test", "SHA256WithRSAEncryption", 365, null, null);
-        // final KeyPair clientKeyPair = CertUtils.generateRandomKeyPair(1024);
-        // final List<String> domainNames = Arrays.asList("domain1.com", "www.2.domain2.com", "3.domain3.com");
-        // final List<String> addressList = Arrays.asList("1.2.3.4", "192.168.1.1", "2a02:120b:2c16:f6d0:d9df:8ebc:e44a:f181");
-
-        // final X509Certificate clientCert = CertUtils.generateV3Certificate(caCertificate, caKeyPair, clientKeyPair.getPublic(),
-        //         "CN=domain.example", "SHA256WithRSAEncryption", 10, domainNames, addressList);
-
-        // clientCert.verify(caKeyPair.getPublic());
-        // if (clientCert.getIssuerDN().equals(caCertificate.getIssuerDN()) && clientCert.getSigAlgName().equalsIgnoreCase("SHA256WITHRSA")) {
-        //     resultMap.put("certificate", "true");
-        // } else {
-        //     resultMap.put("certificate", "false");
-        // }
-        // Encrypt (보안 인증 기능에 사용되는 암복호화가 동작하는지 확인)
+        // Encrypt (보안기능에 적합한 알고리즘으로 암호화된 암호가 정상적으로 복호화되는지 확인)
         final String pwd = "managementkey";
         AeadBase64Encryptor encryptor = new AeadBase64Encryptor(pwd.getBytes(StandardCharsets.UTF_8));
         try {
-            // final String decrypt = encryptor.decrypt("DlTJUG8rWFjOd3aoHtbBGEcQ/piovBzRJ/bnQ1FACLg=");
-            final String decrypt = encryptor.decrypt("piovBzRJ/bnQ1FACLg=");
+            final String decrypt = encryptor.decrypt("DlTJUG8rWFjOd3aoHtbBGEcQ/piovBzRJ/bnQ1FACLg=");
             if (decrypt.equalsIgnoreCase("mold")) {
                 resultMap.put("encrypt", "true");
             } else {
