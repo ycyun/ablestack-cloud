aaa11bcfa86e354c8cb8d4dc70abba75dde496a8
Dajeong-Park <ekwjd7182@gmail.com>
2023-09-19 15:33:46 +0900

    코드 정리

--- utils/src/main/java/com/cloud/utils/mold/SecurityCheck.java
+++ utils/src/main/java/com/cloud/utils/mold/SecurityCheck.java
@@ -19,23 +19,16 @@
 
 package com.cloud.utils.mold;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
-import java.security.NoSuchAlgorithmException;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.apache.cloudstack.utils.security.DigestHelper;
-
-import com.amazonaws.util.StringInputStream;
 import com.cloud.utils.StringUtils;
 import com.cloud.utils.crypt.AeadBase64Encryptor;
 import com.cloud.utils.crypt.EncryptionException;
 
 public class SecurityCheck {
-    public static void main(String[] args) throws UnsupportedEncodingException, EncryptionException, NoSuchAlgorithmException, IOException {
+    public static void main(String[] args) throws EncryptionException {
         Map<String, String> resultMap = new HashMap<>();
         // Request (Request 및 Response에 포함된 민감한 문자열을 제거하는 프로세스가 정상적으로 동작하는지 확인)
         final String input = "name=SS1&provider=SMB&zoneid=5a60af2b-3025-4f2a-9ecc-8e33bf2b94e3&url=cifs%3A%2F%2F10.102.192.150%2FSMB-Share%2Fsowmya%2Fsecondary%3Fuser%3Dsowmya%26password%3DXXXXX%40123%26domain%3DBLR";
@@ -59,20 +52,7 @@ public class SecurityCheck {
         } catch (EncryptionException e){
             resultMap.put("encrypt", "false");
         }
-        // checksum
-        try {
-            final String inputStr = "01234567890123456789012345678901234567890123456789012345678901234567890123456789\n";
-            final String sha256Checksum = "{SHA-256}c6ab15af7842d23d3c06c138b53a7d09c5e351a79c4eb3c8ca8d65e5ce8900ab";
-            InputStream inputStream = new StringInputStream(inputStr);
-            String digest = DigestHelper.digest("SHA-256", inputStream).toString();
-            if (sha256Checksum.equals(digest)) {
-                resultMap.put("digest", "true");
-            } else {
-                resultMap.put("digest", "false");
-            }
-        } catch (UnsupportedEncodingException e) {
-            resultMap.put("digest", "false");
-        }
+        // certificate, checksum, sshkey fingerprint digest, session key, password 항목 테스트시 NoClassDefFoundError로 제외
         System.out.printf("%s%n", resultMap);
     }
 }
