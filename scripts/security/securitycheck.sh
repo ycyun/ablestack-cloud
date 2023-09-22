cb3deb46fa2f289373727dab431b839bd2264887
Dajeong-Park <ekwjd7182@gmail.com>
2023-09-22 12:16:14 +0900

    자체시험 테스트 실행 스크립트 수정

--- scripts/security/securitycheck.sh
+++ scripts/security/securitycheck.sh
@@ -17,10 +17,58 @@
 # under the License.
 
 # Security Check 
-# 보안 기능 프로세스가 정상적으로 작동하는지 확인하는 스크립트
-# 항목 : encrypt, request
+# JAVA로 작성된 Test 코드를 실행하는 Junit.jar 파일을 사용하여 보안 기능과 관련된 Utils 프로세스가 정상적으로 작동하는지 확인하는 스크립트
+# 항목 : string, encrypt(db), encrypt(rsa), password, sshkey, http
 # return : {이름, 결과}
 
-jarfile='/usr/share/cloudstack-common/lib/cloudstack-utils.jar'
-result=$(java -classpath $jarfile com.cloud.utils.mold.SecurityCheckUtil)
+jarfile='/usr/share/cloudstack-common/lib/'
+cmd=${jarfile}junit-4.13.2.jar:${jarfile}hamcrest-all-1.3.jar:${jarfile}cloudstack-utils-test.jar:${jarfile}cloudstack-utils.jar
+
+# String 유틸리티 
+result=$(java -classpath $cmd org.junit.runner.JUnitCore com.cloud.utils.StringUtilsTest | grep -i OK)
 echo $result
+if [ -n "$result" ]; then
+    echo "string,true"
+else
+    echo "string,false"
+fi
+
+# encrypt(db) 유틸리티 
+result=$(java -classpath $cmd org.junit.runner.JUnitCore com.cloud.utils.crypt.EncryptionSecretKeyCheckerTest | grep -i OK)
+if [ -n "$result" ]; then
+    echo "encrypt(db),true"
+else
+    echo "encrypt(db),false"
+fi
+
+# encrypt(ras) 유틸리티 
+result=$(java -classpath $cmd:${jarfile}bcprov-jdk15on-1.70.jar org.junit.runner.JUnitCore com.cloud.utils.crypto.RSAHelperTest | grep -i OK)
+if [ -n "$result" ]; then
+    echo "encrypt(rsa),true"
+else
+    echo "encrypt(rsa),false"
+fi
+
+# password 유틸리티 
+result=$(java -classpath $cmd:${jarfile}bcprov-jdk15on-1.70.jar org.junit.runner.JUnitCore com.cloud.utils.PasswordGeneratorTest | grep -i OK)
+if [ -n "$result" ]; then
+    echo "password,true"
+else
+    echo "password,false"
+fi
+
+# sshkey 유틸리티 
+result=$(java -classpath $cmd:${jarfile}jsch-0.1.55.jar org.junit.runner.JUnitCore com.cloud.utils.ssh.SSHKeysHelperTest | grep -i OK)
+if [ -n "$result" ]; then
+    echo "sshkey,true"
+else
+    echo "sshkey,false"
+fi
+
+# http 유틸리티 
+result=$(java -classpath $cmd:${jarfile}javax.servlet-api-4.0.1.jar:${jarfile}spring-test-5.3.26.jar:${jarfile}spring-core-5.3.26.jar:${jarfile}commons-logging-1.2.jar org.junit.runner.JUnitCore com.cloud.utils.HttpUtilsTest | grep -i OK)
+if [ -n "$result" ]; then
+    echo "http,true"
+else
+    echo "http,false"
+fi
