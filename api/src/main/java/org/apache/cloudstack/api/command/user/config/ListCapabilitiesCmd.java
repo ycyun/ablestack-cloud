6304943cf6763f21ef673958c4373e5de26de4a3
JS Choi <77760789+jschoiRR@users.noreply.github.com>
2023-10-04 11:54:24 +0900

    20231004 일자 소스 병합 후 빌드 오류 수정

--- api/src/main/java/org/apache/cloudstack/api/command/user/config/ListCapabilitiesCmd.java
+++ api/src/main/java/org/apache/cloudstack/api/command/user/config/ListCapabilitiesCmd.java
@@ -23,6 +23,7 @@ import org.apache.cloudstack.api.ApiConstants;
 import org.apache.cloudstack.api.BaseCmd;
 import org.apache.cloudstack.api.response.CapabilitiesResponse;
 import org.apache.cloudstack.config.ApiServiceConfiguration;
+import org.apache.log4j.Logger;
 
 import com.cloud.user.Account;
 
@@ -30,7 +31,6 @@ import com.cloud.user.Account;
         requestHasSensitiveInfo = false, responseHasSensitiveInfo = false)
 public class ListCapabilitiesCmd extends BaseCmd {
     public static final Logger s_logger = Logger.getLogger(ListCapabilitiesCmd.class.getName());
-
 
     @Override
     public long getEntityOwnerId() {
