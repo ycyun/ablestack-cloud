5d57ffd97e9596bec14b235867850831e7ba3d1c
JS Choi <77760789+jschoiRR@users.noreply.github.com>
2023-11-07 09:45:48 +0900

    default 언어 한글로 변경/ vm 복제 아이콘 변경

--- ui/src/components/header/TranslationMenu.vue
+++ ui/src/components/header/TranslationMenu.vue
@@ -52,17 +52,17 @@ import moment from 'moment'
 import 'moment/locale/zh-cn'
 import { loadLanguageAsync } from '@/locales'
 
-moment.locale('en')
+moment.locale('ko_KR')
 
 export default {
   name: 'TranslationMenu',
   data () {
     return {
-      language: 'en'
+      language: 'ko_KR'
     }
   },
   mounted () {
-    this.language = this.$localStorage.get('LOCALE') || 'en'
+    this.language = this.$localStorage.get('LOCALE') || 'ko_KR'
     this.setLocale(this.language)
   },
   methods: {
@@ -70,7 +70,7 @@ export default {
     onClick (e) {
       let localeValue = e.key
       if (!localeValue) {
-        localeValue = 'en'
+        localeValue = 'ko_KR'
       }
       this.setLocale(localeValue)
     },
--- ui/src/locales/index.js
+++ ui/src/locales/index.js
@@ -22,8 +22,8 @@ const loadedLanguage = []
 const messages = {}
 
 export const i18n = createI18n({
-  locale: 'en',
-  fallbackLocale: 'en',
+  locale: 'ko_KR',
+  fallbackLocale: 'ko_KR',
   silentTranslationWarn: true,
   messages: messages,
   silentFallbackWarn: true,
@@ -33,7 +33,7 @@ export const i18n = createI18n({
 export function loadLanguageAsync (lang) {
   if (!lang) {
     const locale = vueProps.$localStorage.get('LOCALE')
-    lang = (!locale || typeof locale === 'object') ? 'en' : locale
+    lang = (!locale || typeof locale === 'object') ? 'ko_KR' : locale
   }
   if (loadedLanguage.includes(lang)) {
     return Promise.resolve(setLanguage(lang))
