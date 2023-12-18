98eecaf8a10c34111c7feeb8aed7857ee9b33cf6
JS Choi <77760789+jschoiRR@users.noreply.github.com>
2023-12-18 12:13:00 +0900

    20231218일자 신규 소스 병합 - 빌드 오류 수정

--- plugins/hypervisors/kvm/src/test/java/com/cloud/hypervisor/kvm/resource/wrapper/LibvirtPrepareForMigrationCommandWrapperTest.java
+++ plugins/hypervisors/kvm/src/test/java/com/cloud/hypervisor/kvm/resource/wrapper/LibvirtPrepareForMigrationCommandWrapperTest.java
@@ -30,8 +30,6 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.Spy;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.HashMap;
