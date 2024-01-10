f8c8c0bac0a6dfb30d851b4a32f5945815d05267
JS Choi <77760789+jschoiRR@users.noreply.github.com>
2024-01-10 16:41:03 +0900

    빌드 오류 수정2

--- plugins/storage/volume/linstor/src/main/java/com/cloud/hypervisor/kvm/storage/LinstorStoragePool.java
+++ plugins/storage/volume/linstor/src/main/java/com/cloud/hypervisor/kvm/storage/LinstorStoragePool.java
@@ -215,6 +215,16 @@ public class LinstorStoragePool implements KVMStoragePool {
     }
 
     @Override
+    public String createRbdHeartBeatCommand(HAStoragePool primaryStoragePool, String hostPrivateIp, boolean hostValidation, String heartBeatPathRbd) {
+        return null;
+    }
+
+    @Override
+    public String createClvmHeartBeatCommand(HAStoragePool primaryStoragePool, String hostPrivateIp, boolean hostValidation, String heartBeatPathClvm, long heartBeatUpdateTimeout) {
+        return null;
+    }
+
+    @Override
     public String getStorageNodeId() {
         return null;
     }
@@ -228,4 +238,15 @@ public class LinstorStoragePool implements KVMStoragePool {
     public Boolean vmActivityCheck(HAStoragePool pool, HostTO host, Duration activityScriptTimeout, String volumeUUIDListString, String vmActivityCheckPath, long duration) {
         return null;
     }
+
+    @Override
+    public Boolean vmActivityRbdCheck(HAStoragePool pool, HostTO host, Duration activityScriptTimeout, String volumeUUIDListString, String vmActivityCheckPath, long duration) {
+        return null;
+    }
+
+    @Override
+    public Boolean vmActivityClvmCheck(HAStoragePool pool, HostTO host, Duration activityScriptTimeout, String volumeUUIDListString, String vmActivityCheckPath, long duration) {
+        return null;
+    }
+
 }
