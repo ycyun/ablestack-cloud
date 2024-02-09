5b2962f556f2a090582d44929db0c2f1d9d6f61a
Daniel Augusto Veronezi Salvador <38945620+GutoVeronezi@users.noreply.github.com>
2024-02-09 05:35:34 -0300

    Enable over provisioning for SharedMountPoint primary storages (#8481)
    * Enable over provisioning for SharedMountPoint primary storages
    
    * Fix unit tests
    
    * Fix typos and small adjusts
    
    ---------
    
    Co-authored-by: Daniel Augusto Veronezi Salvador <gutoveronezi@apache.org>

--- api/src/main/java/com/cloud/storage/Storage.java
+++ api/src/main/java/com/cloud/storage/Storage.java
@@ -178,7 +178,7 @@ public class Storage {
 
         private final String name;
         private final boolean shared;
-        private final boolean overprovisioning;
+        private final boolean overProvisioning;
         private final boolean encryption;
 
         /**
@@ -202,7 +202,7 @@ public class Storage {
         public StoragePoolType(String name, boolean shared, boolean overprovisioning, boolean encryption) {
             this.name = name;
             this.shared = shared;
-            this.overprovisioning = overprovisioning;
+            this.overProvisioning = overProvisioning;
             this.encryption = encryption;
             addStoragePoolType(this);
         }
@@ -212,10 +212,12 @@ public class Storage {
         }
 
         public boolean supportsOverProvisioning() {
-            return overprovisioning;
+            return overProvisioning;
         }
 
-        public boolean supportsEncryption() { return encryption; }
+        public boolean supportsEncryption() {
+             return encryption; 
+        }
 
         private static void addStoragePoolType(StoragePoolType storagePoolType) {
             map.putIfAbsent(storagePoolType.name, storagePoolType);
@@ -261,7 +263,7 @@ public class Storage {
     }
 
     public static List<StoragePoolType> getNonSharedStoragePoolTypes() {
-        List<StoragePoolType> nonSharedStoragePoolTypes = new ArrayList<StoragePoolType>();
+        List<StoragePoolType> nonSharedStoragePoolTypes = new ArrayList<>();
         for (StoragePoolType storagePoolType : StoragePoolType.values()) {
             if (!storagePoolType.isShared()) {
                 nonSharedStoragePoolTypes.add(storagePoolType);
