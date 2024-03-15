0596299276789304d8bf4878ccf3061f381e47c4
Abhishek Kumar <abhishek.mrt22@gmail.com>
2024-03-15 15:18:54 +0530

    Merge remote-tracking branch 'apache/4.19'

--- usage/src/main/java/com/cloud/usage/UsageManagerImpl.java
+++ usage/src/main/java/com/cloud/usage/UsageManagerImpl.java
@@ -1070,7 +1070,7 @@ public class UsageManagerImpl extends ManagerBase implements UsageManager, Runna
                 createBackupEvent(event);
             }
         } catch (EntityExistsException e) {
-            s_logger.warn(String.format("Failed to create usage event id: %d type: %s due to %s", event.getId(), eventType, e.getMessage()), e);
+            logger.warn(String.format("Failed to create usage event id: %d type: %s due to %s", event.getId(), eventType, e.getMessage()), e);
         }
     }
 
