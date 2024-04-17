1b2340777c8df1dcdc29a862c360078321fd428a
Vishesh <vishesh92@gmail.com>
2024-04-17 17:27:27 +0530

    Fix error message for checkVolume command (#8842)

--- server/src/main/java/com/cloud/storage/VolumeApiServiceImpl.java
+++ server/src/main/java/com/cloud/storage/VolumeApiServiceImpl.java
@@ -1905,7 +1905,8 @@ public class VolumeApiServiceImpl extends ManagerBase implements VolumeApiServic
                 } else if (jobResult instanceof ResourceAllocationException) {
                     throw (ResourceAllocationException)jobResult;
                 } else if (jobResult instanceof Throwable) {
-                    throw new RuntimeException("Unexpected exception", (Throwable)jobResult);
+                    Throwable throwable = (Throwable) jobResult;
+                    throw new RuntimeException(String.format("Unexpected exception: %s", throwable.getMessage()), throwable);
                 }
             }
 
